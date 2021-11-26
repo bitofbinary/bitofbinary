4fd133d3d9d03bc31114da76f5a7b1edf52784e1
Deepak <wmwmwmwm@www.com>
2021-11-26 13:19:35 +1100

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
             src: "img/logo.svg",
           },
           items: [
-            { to: "/blog", label: "Blog", position: "left" },
+            // { to: "/blog", label: "Blog", position: "left" },
             { to: "/sites-i-like", label: "Sites I Like", position: "left" },
             { to: "/about", label: "About", position: "right" },
             // {
